589e7d814ea0773217d4ebfa42e90519559be843
Rudra-Parsaniya <parsaniyarudra0018@gmail.com>
2025-08-02 20:11:08 +0530

    api change

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://localhost:7159',
+        target: 'http://localhost:5088',
         changeOrigin: true,
         secure: false,
       },
